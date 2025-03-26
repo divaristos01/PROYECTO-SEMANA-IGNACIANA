b6e0ff07286030fc7a8a6f3a139acb5ef969cd73
divaristos01 <diegoevaristosantiago.guadalupe@alumnado.fundacionloyola.net>
2025-03-26 08:25:39 +0100

    PROYECTO SEMANA IGNACIANA

---
+++ LLMM/SEMANA IGNACIANA/listarJesuitas.php
@@ -0,0 +1,19 @@
+<?php
+//Conecxion con la base de datos
+    include 'configdb.php'; //Traemos los datos para poder conectar
+	$conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); //Nos conectamos a la base de datos
+    $conexion->set_charset("utf8"); 
+	//Desactivamos los errores
+	$controlador = new mysqli_driver();
+    $controlador->report_mode = MYSQLI_REPORT_OFF;
+
+//Aquí escribimos la pregunta que queremos hacerle a la base de datos
+	$sql="SELECT     ";
+	echo $sql;
+	$resultado=$conexion->query($sql); //Busca en la base de datos
+	echo <h1>LISTADO DE JESUITAS</h1> 
+	
+//Cierre de conexion
+	$conexion->close();
+?>
+
